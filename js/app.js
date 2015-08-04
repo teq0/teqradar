6b93dd8e4c838d48764fd174c1af56ad83fd2693
teq0 <teqzero@gmail.com>
2015-08-04 13:02:36 +1000

    Randomise distribution, highlight active when hovered.

--- js/app.js
+++ js/app.js
@@ -21,8 +21,12 @@ d3.helper.tooltip = function(accessor){
             // Add text using the accessor function
             var tooltipText = accessor(d, i) || '';
             // Crop text arbitrarily
-            //tooltipDiv.style('width', function(d, i){return (tooltipText.length > 80) ? '300px' : null;})
-            //    .html(tooltipText);
+            tooltipDiv.style('width', function(d, i){return (tooltipText.length > 80) ? '300px' : null;})
+                .html(tooltipText);
+
+            d3.select(d.symbol).classed('selectedItem', true);
+            d3.select('svg').classed('mute', true);
+
         })
             .on('mousemove', function(d, i) {
                 // Move tooltip
@@ -35,108 +39,90 @@ d3.helper.tooltip = function(accessor){
             .on("mouseout", function(d, i){
                 // Remove tooltip
                 tooltipDiv.remove();
+                d3.select(d.symbol).classed('selectedItem', false);
+                d3.select('svg').classed('mute', false);
             });
 
     };
 };
 
+// Lorem ipsum generator
+// @author C. Peter Chen of http://dev-notes.com
+// @date 20080812
+function loremIpsum(elem) {
+    var loremIpsumWordBank = new Array("lorem","ipsum","dolor","sit","amet,","consectetur","adipisicing","elit,","sed","do","eiusmod","tempor","incididunt","ut","labore","et","dolore","magna","aliqua.","enim","ad","minim","veniam,","quis","nostrud","exercitation","ullamco","laboris","nisi","ut","aliquip","ex","ea","commodo","consequat.","duis","aute","irure","dolor","in","reprehenderit","in","voluptate","velit","esse","cillum","dolore","eu","fugiat","nulla","pariatur.","excepteur","sint","occaecat","cupidatat","non","proident,","sunt","in","culpa","qui","officia","deserunt","mollit","anim","id","est","laborum.","sed","ut","perspiciatis,","unde","omnis","iste","natus","error","sit","voluptatem","accusantium","doloremque","laudantium,","totam","rem","aperiam","eaque","ipsa,","quae","ab","illo","inventore","veritatis","et","quasi","architecto","beatae","vitae","dicta","sunt,","explicabo.","nemo","enim","ipsam","voluptatem,","quia","voluptas","sit,","aspernatur","aut","odit","aut","fugit,","sed","quia","consequuntur","magni","dolores","eos,","qui","ratione","voluptatem","sequi","nesciunt,","neque","porro","quisquam","est,","qui","dolorem","ipsum,","quia","dolor","sit,","amet,","consectetur,","adipisci","velit,","sed","quia","non","numquam","eius","modi","tempora","incidunt,","ut","labore","et","dolore","magnam","aliquam","quaerat","voluptatem.","ut","enim","ad","minima","veniam,","quis","nostrum","exercitationem","ullam","corporis","suscipit","laboriosam,","nisi","ut","aliquid","ex","ea","commodi","consequatur?","quis","autem","vel","eum","iure","reprehenderit,","qui","in","ea","voluptate","velit","esse,","quam","nihil","molestiae","consequatur,","vel","illum,","qui","dolorem","eum","fugiat,","quo","voluptas","nulla","pariatur?","at","vero","eos","et","accusamus","et","iusto","odio","dignissimos","ducimus,","qui","blanditiis","praesentium","voluptatum","deleniti","atque","corrupti,","quos","dolores","et","quas","molestias","excepturi","sint,","obcaecati","cupiditate","non","provident,","similique","sunt","in","culpa,","qui","officia","deserunt","mollitia","animi,","id","est","laborum","et","dolorum","fuga.","harum","quidem","rerum","facilis","est","et","expedita","distinctio.","Nam","libero","tempore,","cum","soluta","nobis","est","eligendi","optio,","cumque","nihil","impedit,","quo","minus","id,","quod","maxime","placeat,","facere","possimus,","omnis","voluptas","assumenda","est,","omnis","dolor","repellendus.","temporibus","autem","quibusdam","aut","officiis","debitis","aut","rerum","necessitatibus","saepe","eveniet,","ut","et","voluptates","repudiandae","sint","molestiae","non","recusandae.","itaque","earum","rerum","hic","tenetur","a","sapiente","delectus,","aut","reiciendis","voluptatibus","maiores","alias","consequatur","aut","perferendis","doloribus","asperiores","repellat");
+    var minWordCount = 15;
+    var maxWordCount = 100;
+
+    var randy = Math.floor(Math.random()*(maxWordCount - minWordCount)) + minWordCount;
+    var ret = "";
+    for(i = 0; i < randy; i++) {
+        var newTxt = loremIpsumWordBank[Math.floor(Math.random() * (loremIpsumWordBank.length - 1))];
+        if (ret.substring(ret.length-1,ret.length) == "." || ret.substring(ret.length-1,ret.length) == "?") {
+            newTxt = newTxt.substring(0,1).toUpperCase() + newTxt.substring(1, newTxt.length);
+        }
+        ret += " " + newTxt;
+    }
+
+    return ret;
+}
 
 (function() {
 
     var dataRepo = function() {
-        function getRadarData() {
-            // TODO - read from somewhere
+
+        function genDummyData(minItems, maxItems) {
+
+            minItems = minItems || 35;
+            maxItems = maxItems || minItems;
+            maxItems = Math.max(minItems, maxItems);
+
+            var numItems = (minItems != maxItems) ? Math.floor(Math.random() * (maxItems - minItems)) + minItems : maxItems;
+
+            var cfg = displayManager().getRadarConfig();
             var data = {
-                items: [
-                    {
-                        name: 'ASP.NET MVC',
-                        status: 'Adopt',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'ReactJS',
-                        status: 'Assess',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'Angular 2.0',
-                        status: 'Assess',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'Go',
-                        status: 'Trial',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'JSON API',
-                        status: 'Assess',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'JSON API',
-                        status: 'Assess',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'JSON API',
-                        status: 'Assess',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'JSON API',
-                        status: 'Assess',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'JSON API',
-                        status: 'Assess',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'JSON API',
-                        status: 'Assess',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'JSON API',
-                        status: 'Assess',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'JSON API',
-                        status: 'Assess',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'JSON API',
-                        status: 'Assess',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'JSON API',
-                        status: 'Assess',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'JSON API',
-                        status: 'Assess',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'JSON API',
-                        status: 'Assess',
-                        category: 'Languages and Frameworks'
-                    },
-                    {
-                        name: 'ASP.NET Web forms',
-                        status: 'Avoid',
-                        category: 'Languages and Frameworks'
-                    }
-                ]
+                items: []
             };
 
+            function genValue(arr) {
+                return arr[Math.floor(Math.random() * arr.length)];
+            }
+
+            var names = [
+                'ASP.NET MVC',
+                'JSON API',
+                'React JS',
+                'Swift',
+                'Docker',
+                'Ionic',
+                'Xamarin',
+                'Reverse Conway',
+                'Angular 2.0',
+                'React Mobile',
+                'Golang',
+                'Neo4j'
+            ];
+
+            numItems = numItems || 35;
+
+            for (var idx = 0; idx < numItems; idx++) {
+                var item = {
+                    name: genValue(names),
+                    status: genValue(cfg.statuses),
+                    category: 'Languages and Frameworks',
+                    description: loremIpsum()
+                };
+
+                data.items.push(item);
+            }
+
             return data;
+        }
+
+        function getRadarData() {
+
+            // TODO - read data from somewhere
+            return genDummyData(18, 30);
         }
 
         return {
@@ -152,10 +138,10 @@ d3.helper.tooltip = function(accessor){
             xMargin: 10,
             yMargin: 10,
             statuses: [
-                'Avoid',
-                'Assess',
+                'Adopt',
                 'Trial',
-                'Adopt'
+                'Assess',
+                'Avoid'
             ],
             quadrants: [
                 'Languages and frameworks',
@@ -198,37 +184,69 @@ function Radar(dataRepo, displayManager) {
         PI: Math.PI,
         PI_ON_2: Math.PI/2,
         PI_ON_4: Math.PI/4,
+        PI_ON_8: Math.PI/8,
         THREE_PI_ON_2: 3 * Math.PI/2,
-        PI_ON_32: Math.PI/32
+        PI_ON_16: Math.PI/16
     };
 
     function displayRadar() {
 
         var radarRoot = dMgr.getRadarElement();
         var cfg = dMgr.getRadarConfig();
-        var width = cfg.width - cfg.xMargin;
-        var height = cfg.height - cfg.yMargin;
-        var maxRadius = Math.sqrt((width*width) + (height*height));
-        var ringWidth = maxRadius/(cfg.statuses.length + 2);
+
+        // get the physical size of the canvas
+        var svgWidth = cfg.width - cfg.xMargin;
+        var svgHeight = cfg.height - cfg.yMargin;
+        var rWidth = 1000;
+        var rHeight = 1000;
+
+        // get a scale object so we can work normalised to 1000x1000
+
+        var maxRadius = Math.sqrt((rWidth*rWidth) + (rHeight*rHeight));
+        var rScale = d3.scale.linear()
+            .domain([0, maxRadius])
+            .range([0, Math.sqrt(svgWidth*svgHeight)]);
+
+        // one ring per status
         var rings = [];
 
         var svg = radarRoot.append("svg")
-            .attr("width", cfg.width)
-            .attr("height", cfg.height);
+            .attr("width", svgWidth)
+            .attr("height", svgHeight);
 
         var g = svg.append("g")
-            .attr("transform", "translate(" + cfg.width + "," + cfg.height + ")");
+            .attr("transform", "translate(" + svgWidth + "," + svgHeight + ")");
 
-        for (var idxStatus = 0, ringRadius = 1; idxStatus < cfg.statuses.length; idxStatus++, ringRadius += ringWidth) {
-            rings.push({
-                innerRadius: ringRadius,
-                outerRadius: ringRadius + ringWidth  - 1,
-                avgRadius: ringRadius + (ringWidth/2),
-                name: cfg.statuses[idxStatus],
+        // calculate the ring radii
+        var innerRadius = 0, outerRadius = 500;
+
+        _.forEach(cfg.statuses, function(status) {
+
+            var ring = {
+                innerRadius: innerRadius,
+                outerRadius: outerRadius,
+                ringWidth: outerRadius - innerRadius,
+                name: status,
                 itemCount: 0,
                 totalItems: 0
-            });
-        }
+            };
+
+            innerRadius = outerRadius;
+            outerRadius += ring.ringWidth / Math.SQRT2;
+
+            rings.push(ring);
+        });
+
+        // scale the width of the rings
+        var ringScale = d3.scale.linear()
+            .domain([0, rings[rings.length - 1].outerRadius])
+            .range([0, maxRadius]);
+
+        _.forEach(rings, function(ring, idx) {
+            ring.innerRadius = ringScale(ring.innerRadius);
+            ring.outerRadius = ringScale(ring.outerRadius);
+            ring.ringWidth = ring.outerRadius - ring.innerRadius;
+        });
 
         var data = dRepo.getRadarData();
 
@@ -243,37 +261,76 @@ function Radar(dataRepo, displayManager) {
             }
         });
 
+        _.forEach(rings, function(ring, idx) {
+            // get the radial angle between items
+            ring.itemTheta = (radians.PI_ON_2 * 0.9) / ring.totalItems;
+
+            // TODO - if itemTheta is too small for the ring's radius things will collide, and we'll need two bands of items
+        });
+
+        var symbolSize = 800;
         var circles = svg.selectAll("path")
                 .data(data.items)
             .enter()
                 .append("path")
                 .attr("transform", function(d) {
-                    var theta = d.ring.itemCount++ * radians.PI_ON_32;
-                    var x = cfg.width - (d.ring.avgRadius * Math.cos(theta)) - cfg.xMargin;
-                    var y = cfg.height - (d.ring.avgRadius * Math.sin(theta)) - cfg.yMargin;
 
-                    return "translate(" + x + "," + y + ")";
+                    // distribute the items around the diagonal
+                    var theta = radians.PI_ON_4 + ((((d.ring.itemCount & 1) << 1) -1) * ((d.ring.itemCount + 1) >> 1) * d.ring.itemTheta) + ((Math.random() - 0.5) * radians.PI_ON_16);
+                    d.ring.itemCount++;
+
+                    var radius = d.ring.innerRadius + (d.ring.ringWidth / 2) + (1 + ((Math.random() - 0.5)) * Math.sqrt(d.ring.ringWidth) * 5);
+
+                    d.x = svgWidth - (rScale(radius) * Math.cos(theta)) - cfg.xMargin;
+                    d.y = svgHeight - (rScale(radius) * Math.sin(theta)) - cfg.yMargin;
+
+                    return "translate(" + d.x + "," + d.y + ")";
                 })
                 .attr("class", "radarItem")
-                .attr("d", d3.svg.symbol().type('triangle-up'))
-                .attr('size', 200)
-                .call(d3.helper.tooltip(
-                    function(d, i){
-                        return "<b>Name: " +d.name + "</b><br/>Status: "+ d.status;
-                    }
-                ));
+                .attr("d", d3.svg.symbol().size(rScale(symbolSize))
+                .type('triangle-up'))
+                .each(function(d, i) {
+                        // I wonder if this causes a circular reference?
+                        d.symbol = this;
+                });
+
+
+        //Add the SVG Text Element to the svgContainer
+        var text = svg.selectAll("text")
+                                .data(data.items)
+                                .enter()
+                                .append("text");
+
+        var textXOffset = rScale(-4.2);
+        var textYOffset = rScale(13);
+
+        //Add SVG Text Element Attributes
+        var textLabels = text
+             .attr("x", function(d) { return d.x + (textXOffset * ((d.idx+1).toString().length + 1)); })
+             .attr("y", function(d) { return d.y + textYOffset; })
+             .text( function (d) { return d.idx + 1; })
+             .attr("font-family", "sans-serif")
+             .attr("font-size", "8px")
+             .attr("fill", "white")
+             .attr('text-align', 'center')
+            .call(d3.helper.tooltip(
+                function(d, i){
+                    return "<b>" + (d.idx + 1) +": " + d.name + "</b><br/><em>" + d.status + "</em><br/><br/>" + d.description;
+                }
+            ));
+
 
         for (var idxRing = 0; idxRing < rings.length; idxRing++) {
             var ring = rings[idxRing];
 
             var arc = d3.svg.arc()
-                .innerRadius(ring.innerRadius)
-                .outerRadius(ring.outerRadius)
+                .innerRadius(rScale(ring.innerRadius))
+                .outerRadius(rScale(ring.outerRadius))
                 .startAngle(radians.THREE_PI_ON_2)
                 .endAngle(2 * Math.PI);
 
             g.append("path")
-                .attr("class", "arc")
+                .attr("class", "arc" + idxRing)
                 .attr("d", arc);
         }
     }
